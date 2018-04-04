1ee2bccbc14a2febe0263d0f90f0477758380e0f
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-04 15:02:45 +0100

    Implemented regex by opening file and looping through all lines to find specified word in text

---
+++ 04lesson.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+	my $file = 'mymanjeeves.txt';
+	open(INPUT, $file) or	die("input file $file not found.\n");
+	while(my $line = <INPUT>){
+	if($line =~ / egg /) {
+		print "--------- $line";	
+	}	
+	}
+	close(INPUT);
+}
+
+main();
