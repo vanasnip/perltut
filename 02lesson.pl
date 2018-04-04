a7ffaf40feb224eb809de2fe843f0f332705427a
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-04 11:36:11 +0100

    downloading images over http not https

--- 02lesson.pl
+++ 02lesson.pl
@@ -6,7 +6,13 @@ use LWP::Simple;
 sub main {
 	print "Downloading...\n";
 	#print get("http://www.ivandash.com/");
-	getstore("http://www.ivandash.com/","lsn02_download.html");
+	
+	#getstore("http://www.ivandash.com/","lsn02_download.html");
+	# location url, filename
+	
+	getstore("http://ivandash.com/images/01html.png","mahmuhmia.png");
+	#using single quotes to tell perl that just treat it as a string	
+	#this does not work over https which unfortunately is everywhere now, stackoverflow it!
 	print "finished\n";
 }
 
