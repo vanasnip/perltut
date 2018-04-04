b3ad1d0534c9481160a17c459076148f74a3ad01
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-04 14:02:32 +0100

    Working with perl array, looping using foreach

--- 03lesson.pl
+++ 03lesson.pl
@@ -2,13 +2,26 @@ use strict;
 use warnings;
 use LWP::Simple;
 
+$|=1;# turns off output buffering
+
 sub main {
-	my $file = 'mahmuhmia.jpg';
 	
-	if(-f $file){
-		print "Found file $file\n";
-	} else {
-		print "File $file not found\n";
+	my @files = (
+		'mahmuhmia.jpg',
+		'mahmuhmia02.jpg',
+		'mahmuhmia03.jpg',
+		'mahmuhmia04.jpg',
+		'mahmuhmia05.jpg',
+		'mahmuhmia07.jpg',
+	);
+
+	foreach my $file(@files) {
+		print "$file\n";
+		if(-f $file){
+			print "Found file $file\n";
+		} else {
+			print "File $file not found\n";
+		}
 	}
 }
 
