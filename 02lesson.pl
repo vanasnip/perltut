9316f66a3015006de05830addc27fd69cc94d12a
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-04 11:52:52 +0100

    Showcasing decalring variables and if statements

--- 02lesson.pl
+++ 02lesson.pl
@@ -10,10 +10,18 @@ sub main {
 	#getstore("http://www.ivandash.com/","lsn02_download.html");
 	# location url, filename
 	
-	getstore("http://ivandash.com/images/01html.png","mahmuhmia.png");
+	#getstore("http://ivandash.com/images/01html.png","mahmuhmia.png");
 	#using single quotes to tell perl that just treat it as a string	
 	#this does not work over https which unfortunately is everywhere now, stackoverflow it!
-	print "finished\n";
+	
+	my $code = getstore("http://ivandash.com/images/01html.png","mahmuhmia.png");
+	if($code == 200){
+		print "Success\n";
+	}
+	else {
+		print "Failed\n";
+	}
+	print "Finished\n";
 }
 
 main();
